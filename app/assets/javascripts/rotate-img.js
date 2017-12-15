f1120bd09dfe5b021edac855fa4948ef915bfdf6
shouta-sugawara <hone-ta@amber.plala.or.jp>
2017-12-15 16:24:45 +0900

    fix

--- app/assets/javascripts/rotate-img.js
+++ app/assets/javascripts/rotate-img.js
@@ -1,15 +1,15 @@
 $(function(){
     resizeTopImage();
     $('#top-img .images img:gt(0)').hide();
-    $('#top-img .messages .message:gt(0)').hide();
+    $('#top-img #messages .message:gt(0)').hide();
     debugger;
     setInterval(function(){
       $('#top-img .images :first-child').fadeOut(3000)
          .next('img').fadeIn(3000)
          .end().appendTo('#top-img .images');
-      $('#top-img .messages .message:eq(0)').fadeOut(3000)
+      $('#top-img #messages .message:eq(0)').fadeOut(3000)
          .next('.message').fadeIn(3000)
-         .end().appendTo('#top-img .messages');
+         .end().appendTo('#top-img #messages');
        },
       6000);
 });
